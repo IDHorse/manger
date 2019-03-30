aacdd796f26f11c32d05fbd8ec1e06fff51e963a
IDHorse <13121177622@163.com>
2019-03-30 09:34:02 +0800

    11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux'
 import './App.css';
 
+const mapStateToProps = function(state){
+  return {
+    num: state.counter.num
+  }
+}
+@connect(mapStateToProps)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        this is app
+        this is app {this.props.num}
       </div>
     );
   }
---
+++ src/store/reducers/counter.js
@@ -0,0 +1,9 @@
+
+const initialState = {
+    num: 1
+}
+
+export default function (state=initialState,actions) {  
+
+    return state;
+}
